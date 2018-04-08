7d23e30596bb7d6cc0d322fa5bcd865e23e68007
Alexa <alexa.anderson@outlook.com>
2018-04-08 15:39:44 -0500

    Teaser work

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -8,9 +8,8 @@ $(function () {
   $.get('/api/posts/author/rightactioning', function (response) {
     if (response.status == 'ok') {
       var output = '';
-      $.each(response.items, function (k, item) {
-        output += '<div class="blog-teaser-wrapper">';
-        output += '<div class="blog-post"><header>';
+      $.each(response.items.slice(0, 4), function (k, item) {
+        output += '<div class="blog-teaser-wrapper"><header>';
         var tagIndex = item.description.indexOf('<img');
         var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex;
         var srcStart = srcIndex + 5; // 5 for the length of 'src="'
@@ -20,15 +19,16 @@ $(function () {
         if (src.indexOf('cdn-images-1') < 0) src = 'http://az616578.vo.msecnd.net/files/2016/06/15/636016285941226674637698081_1127.jpg'; // TODO: change to default image when authors in DB
 
         output += '<div class="blog-element">'
-        output += `<a href="${item.link}""><img class="teaser-image image-responsive" src="${src}""></a></div></header>`;
+        output += `<a href="${item.link}" class="teaser-image-link"><img class="teaser-image" src="${src}""></a></div></header>`;
         output += `<div class="blog-content"><h4><a href="${item.link}">${item.title}</a></h4>`;
         output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
         var content = item.description.replace(/<img[^>]*>/g,"");
-        var maxLength = 240;
+        content = content.replace(/<\/?[a-zA-Z0-9]*>/g," ");
+        var maxLength = 100;
         var trimmed = content.substr(0, maxLength);
         trimmed = trimmed.substr(0, Math.min(trimmed.length, trimmed.lastIndexOf(" ")))
-        output += '<p>' + trimmed + '...</p>';
-        output += '</div></div></div>';
+        output += '<p class="teaser-content">' + trimmed + '...</p>';
+        output += '</div></div>';
       });
       $content.html(output);
     }
