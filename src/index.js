9b2688695832263588ea19fdc2978a24f7ca033b
Alexa <alexa.anderson@outlook.com>
2018-05-27 01:03:53 -0500

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-export const Main = () => (
-    <div>
-        This is the app! oh shit!
-    </div>
+export const MainNav = () => (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </ul>
+    </nav>
 );
 
-render(<Main />, document.getElementById('content'));
+export const Home = () => <div>Home!</div>;
+export const Contact = () => <div>Contact</div>;
+
+export const Router = () => (
+    <BrowserRouter >
+        <Fragment>
+            <MainNav />
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+        </Fragment>
+    </BrowserRouter>
+);
+
+render(<Router />, document.getElementById('content'));
